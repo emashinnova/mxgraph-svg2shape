fa5a4c0ff3aa6551cb4efac67d990aa168e36977
emashinnova <eakulkina@ADZHURS.tec>
2021-06-01 12:58:13 +0300

    Задача #267175 [Отчёты] Отчёт "Геологическая колонка" публикуется в редактор графики без линий таблицы и без литологии

--- src/main/java/com/mxgraph/svg2xml/XmlConfig.java
+++ src/main/java/com/mxgraph/svg2xml/XmlConfig.java
@@ -1,287 +1,309 @@
 /**
- * $Id: XmlConfig.java,v 1.6 2013/04/22 05:32:08 mate Exp $
- * Copyright (c) 2013, JGraph
+ * $Id: XmlConfig.java,v 1.6 2013/04/22 05:32:08 mate Exp $ Copyright (c) 2013,
+ * JGraph
  */
 
 package com.mxgraph.svg2xml;
 
-public class XmlConfig 
+
+import java.util.Optional;
+
+
+public class XmlConfig
 {
-	//NOTE: don't set these variables manually, they are read from the XML config files.
-	//these are default values
-	
-	//if true, the first element is placed in the stencils background block
-	private boolean background = false;
-	
-	//true: calculate border 
-	//false: read from source SVG if false
-	private boolean calculateBorder = true;
-	
-	//the bounding box of the SVG
-	private double stencilBoundsX;
-	private double stencilBoundsY;
-	private double stencilBoundsMinX = 0;
-	private double stencilBoundsMinY = 0;
+    // NOTE: don't set these variables manually, they are read from the XML
+    // config files.
+    // these are default values
 
-	//variable or fixed aspect ratio of the current stencil 
-	public static enum aspectType { VARIABLE, FIXED };
-	private aspectType aspect = aspectType.VARIABLE;
-	
-	private String strokewidth = "inherit";
-	
-	//use relative scaling (used if you want to change the size of resulting XMLs)
-	private boolean relativeScaling = true;
+    // if true, the first element is placed in the stencils background block
+    private boolean background = false;
 
-	// scaling ratio used if relativeScaling=true (source SVG / destination XML)
-	private double relativeScalingRatio = 1.0d;
-	
-	//every stencil will be absoluteScalingX in X size, if relativeScaling=false
-	private double absoluteScalingX = 100d;
-	
-	//every stencil will be absoluteScalingY in Y size, if relativeScaling=false
-	private double absoluteScalingY = 100d;
+    // true: calculate border
+    // false: read from source SVG if false
+    private boolean calculateBorder = true;
 
-	//round coords to reduce XML size
-	private boolean roundCoords = true;
-	
-	//round to decimals (used with roundCoords=true)
-	private int decimalsToRound = 2;
-	
-	//NOTE: do not set groupName here, since it is read from the groups config file. add an entry such as "groupname: yourgroupname;" without the quotations to your groups XML (config file)
-	//if null, group name is generated from Svg2xml.stencilUserMarker and folder structure
-	private String groupName = null;
-	
-	private Connection connection;
-	private XmlStyle style;
+    // the bounding box of the SVG
+    private double stencilBoundsX;
+    private double stencilBoundsY;
+    private double stencilBoundsMinX = 0;
+    private double stencilBoundsMinY = 0;
 
-	public XmlConfig(Svg2XmlGui gui)
-	{
-		setCalculateBorder(gui.isCalculateBorder());
-		setRelativeScaling(gui.isRelativeScaling());
-		setRelativeScalingRatio(gui.getRelativeScaleRatio(), gui);
-		setAbsoluteScalingX(gui.getAbsoluteScalingX());
-		setAbsoluteScalingY(gui.getAbsoluteScalingY());
-		setRoundCoords(gui.isRoundCoords());
-		setDecimalsToRound(gui.getDecimalsToRound());
-	}
+    // variable or fixed aspect ratio of the current stencil
+    public static enum aspectType
+    {
+        VARIABLE, FIXED
+    };
 
-	public XmlConfig()
-	{
+    private aspectType aspect = aspectType.VARIABLE;
 
-	}
-	
-	public boolean isBackground()
-	{
-		return background;
-	}
-	
-	public void setBackground(boolean background)
-	{
-		this.background = background;
-	}
-	
-	public boolean isCalculateBorder()
-	{
-		return calculateBorder;
-	}
-	
-	private void setCalculateBorder(boolean calculateBorder) 
-	{
-		this.calculateBorder = calculateBorder;
-	}
-	
-	public aspectType getAspect() 
-	{
-		return aspect;
-	}
+    private String strokewidth = "inherit";
 
-	public String getAspectString() 
-	{
-		if (aspect == aspectType.FIXED)
-		{
-			return "fixed";
-		}
-		else
-		{
-			return "variable";
-		}
-	}
-	
-	public void setAspect(aspectType aspect)
-	{
-		this.aspect = aspect;
-	}
+    // use relative scaling (used if you want to change the size of resulting
+    // XMLs)
+    private boolean relativeScaling = true;
 
-	public void setAspect(String aspect)
-	{
-		if (aspect.toLowerCase().equals("fixed"))
-		{
-			this.aspect = aspectType.FIXED;
-		}
-		else
-		{
-			this.aspect = aspectType.VARIABLE;
-		}
-	}
+    // scaling ratio used if relativeScaling=true (source SVG / destination XML)
+    private double relativeScalingRatio = 1.0d;
 
-	public boolean isRelativeScaling()
-	{
-		return relativeScaling;
-	}
+    // every stencil will be absoluteScalingX in X size, if
+    // relativeScaling=false
+    private double absoluteScalingX = 100d;
 
-	private void setRelativeScaling(boolean relativeScaling) 
-	{
-		this.relativeScaling = relativeScaling;
-	}
+    // every stencil will be absoluteScalingY in Y size, if
+    // relativeScaling=false
+    private double absoluteScalingY = 100d;
 
-	public double getRelativeScalingRatio() 
-	{
-		return relativeScalingRatio;
-	}
+    // round coords to reduce XML size
+    private boolean roundCoords = true;
 
-	private void setRelativeScalingRatio(double relativeScalingRatio, Svg2XmlGui gui) 
-	{
-		if (relativeScalingRatio>0)
-		{
-			if (gui.isRelativeScaling())
-			{
-				this.relativeScalingRatio = relativeScalingRatio;
-			}
-			else
-			{
-				this.relativeScalingRatio = 1.0;
-			}
-		}
-	}
+    // round to decimals (used with roundCoords=true)
+    private int decimalsToRound = 2;
 
-	public double getAbsoluteScalingX() 
-	{
-		return absoluteScalingX;
-	}
+    // NOTE: do not set groupName here, since it is read from the groups config
+    // file. add an entry such as "groupname: yourgroupname;" without the
+    // quotations to your groups XML (config file)
+    // if null, group name is generated from Svg2xml.stencilUserMarker and
+    // folder structure
+    private String groupName = null;
 
-	private void setAbsoluteScalingX(double absoluteScalingX) 
-	{
-		if (absoluteScalingX>0)
-		{
-			this.absoluteScalingX = absoluteScalingX;
-		}
-	}
+    private Connection connection;
+    private XmlStyle style;
 
-	public double getAbsoluteScalingY() 
-	{
-		return absoluteScalingY;
-	}
+    public XmlConfig(Svg2XmlGui gui)
+    {
+        setCalculateBorder(gui.isCalculateBorder());
+        setRelativeScaling(gui.isRelativeScaling());
+        setRelativeScalingRatio(gui.getRelativeScaleRatio(), gui);
+        setAbsoluteScalingX(gui.getAbsoluteScalingX());
+        setAbsoluteScalingY(gui.getAbsoluteScalingY());
+        setRoundCoords(gui.isRoundCoords());
+        setDecimalsToRound(gui.getDecimalsToRound());
+    }
 
-	private void setAbsoluteScalingY(double absoluteScalingY) 
-	{
-		if(absoluteScalingY>0)
-		{
-			this.absoluteScalingY = absoluteScalingY;
-		}
-	}
+    public XmlConfig()
+    {
 
-	public boolean isRoundCoords() 
-	{
-		return roundCoords;
-	}
+    }
 
-	private void setRoundCoords(boolean roundCoords) 
-	{
-		this.roundCoords = roundCoords;
-	}
+    public boolean isBackground()
+    {
+        return background;
+    }
 
-	public int getDecimalsToRound() 
-	{
-		return decimalsToRound;
-	}
+    public void setBackground(boolean background)
+    {
+        this.background = background;
+    }
 
-	private void setDecimalsToRound(int decimalsToRound) 
-	{
-		if (decimalsToRound>0)
-		{
-			this.decimalsToRound = decimalsToRound;
-		}
-		else
-		{
-			this.decimalsToRound = 0;
-		}
-	}
+    public boolean isCalculateBorder()
+    {
+        return calculateBorder;
+    }
 
-	public Connection getConnection() 
-	{
-		return connection;
-	}
+    private void setCalculateBorder(boolean calculateBorder)
+    {
+        this.calculateBorder = calculateBorder;
+    }
 
-	public void setConnection(Connection connection) 
-	{
-		this.connection = connection;
-	}
+    public aspectType getAspect()
+    {
+        return aspect;
+    }
 
-	public XmlStyle getStyle() 
-	{
-		return style;
-	}
+    public String getAspectString()
+    {
+        if (aspect == aspectType.FIXED)
+        {
+            return "fixed";
+        }
+        else
+        {
+            return "variable";
+        }
+    }
 
-	public void setStyle(XmlStyle style) 
-	{
-		this.style = style;
-	}
+    public void setAspect(aspectType aspect)
+    {
+        this.aspect = aspect;
+    }
 
-	public String getStrokewidth()
-	{
-		return strokewidth;
-	}
+    public void setAspect(String aspect)
+    {
+        if (aspect.toLowerCase().equals("fixed"))
+        {
+            this.aspect = aspectType.FIXED;
+        }
+        else
+        {
+            this.aspect = aspectType.VARIABLE;
+        }
+    }
 
-	public void setStrokewidth(String strokewidth)
-	{
-		this.strokewidth = strokewidth;
-	}
+    public boolean isRelativeScaling()
+    {
+        return relativeScaling;
+    }
 
-	public double getStencilBoundsX()
-	{
-		return stencilBoundsX;
-	}
+    private void setRelativeScaling(boolean relativeScaling)
+    {
+        this.relativeScaling = relativeScaling;
+    }
 
-	public void setStencilBoundsX(double stencilBoundsX)
-	{
-		this.stencilBoundsX = stencilBoundsX;
-	}
+    public double getRelativeScalingRatio()
+    {
+        return relativeScalingRatio;
+    }
 
-	public double getStencilBoundsY()
-	{
-		return stencilBoundsY;
-	}
+    private void setRelativeScalingRatio(double relativeScalingRatio,
+        Svg2XmlGui gui)
+    {
+        if (relativeScalingRatio > 0)
+        {
+            if (gui.isRelativeScaling())
+            {
+                this.relativeScalingRatio = relativeScalingRatio;
+            }
+            else
+            {
+                this.relativeScalingRatio = 1.0;
+            }
+        }
+    }
 
-	public void setStencilBoundsY(double stencilBoundsY)
-	{
-		this.stencilBoundsY = stencilBoundsY;
-	}
+    public double getAbsoluteScalingX()
+    {
+        return absoluteScalingX;
+    }
 
-	public double getStencilBoundsMinX()
-	{
-		return stencilBoundsMinX;
-	}
+    private void setAbsoluteScalingX(double absoluteScalingX)
+    {
+        if (absoluteScalingX > 0)
+        {
+            this.absoluteScalingX = absoluteScalingX;
+        }
+    }
 
-	public void setStencilBoundsMinX(double stencilBoundsMinX)
-	{
-		this.stencilBoundsMinX = stencilBoundsMinX;
-	}
+    public double getAbsoluteScalingY()
+    {
+        return absoluteScalingY;
+    }
 
-	public double getStencilBoundsMinY()
-	{
-		return stencilBoundsMinY;
-	}
+    private void setAbsoluteScalingY(double absoluteScalingY)
+    {
+        if (absoluteScalingY > 0)
+        {
+            this.absoluteScalingY = absoluteScalingY;
+        }
+    }
 
-	public void setStencilBoundsMinY(double stencilBoundsMinY)
-	{
-		this.stencilBoundsMinY = stencilBoundsMinY;
-	}
+    public boolean isRoundCoords()
+    {
+        return roundCoords;
+    }
 
-	public String getGroupName() {
-		return groupName;
-	}
+    private void setRoundCoords(boolean roundCoords)
+    {
+        this.roundCoords = roundCoords;
+    }
 
-	public void setGroupName(String groupName) {
-		this.groupName = groupName;
-	}
+    public int getDecimalsToRound()
+    {
+        return decimalsToRound;
+    }
+
+    private void setDecimalsToRound(int decimalsToRound)
+    {
+        if (decimalsToRound > 0)
+        {
+            this.decimalsToRound = decimalsToRound;
+        }
+        else
+        {
+            this.decimalsToRound = 0;
+        }
+    }
+
+    public Connection getConnection()
+    {
+        return connection;
+    }
+
+    public void setConnection(Connection connection)
+    {
+        this.connection = connection;
+    }
+
+    public XmlStyle getStyle()
+    {
+        return style;
+    }
+
+    public void setStyle(XmlStyle style)
+    {
+        this.style = style;
+    }
+
+    public String getStrokewidth()
+    {
+        String strokeFromStyle = Optional.ofNullable(getStyle())
+            .map(s -> s.getStrokeWidth())
+            .filter(w -> w != null && !"".equals(w)).orElse(null);
+
+        return strokeFromStyle != null ? strokeFromStyle : strokewidth;
+    }
+
+    public void setStrokewidth(String strokewidth)
+    {
+        this.strokewidth = strokewidth;
+    }
+
+    public double getStencilBoundsX()
+    {
+        return stencilBoundsX;
+    }
+
+    public void setStencilBoundsX(double stencilBoundsX)
+    {
+        this.stencilBoundsX = stencilBoundsX;
+    }
+
+    public double getStencilBoundsY()
+    {
+        return stencilBoundsY;
+    }
+
+    public void setStencilBoundsY(double stencilBoundsY)
+    {
+        this.stencilBoundsY = stencilBoundsY;
+    }
+
+    public double getStencilBoundsMinX()
+    {
+        return stencilBoundsMinX;
+    }
+
+    public void setStencilBoundsMinX(double stencilBoundsMinX)
+    {
+        this.stencilBoundsMinX = stencilBoundsMinX;
+    }
+
+    public double getStencilBoundsMinY()
+    {
+        return stencilBoundsMinY;
+    }
+
+    public void setStencilBoundsMinY(double stencilBoundsMinY)
+    {
+        this.stencilBoundsMinY = stencilBoundsMinY;
+    }
+
+    public String getGroupName()
+    {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName)
+    {
+        this.groupName = groupName;
+    }
 }
